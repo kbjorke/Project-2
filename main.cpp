963aee36d9fa1f9a7f696643f9dd4867e3ef8397
Kristian Bjørke <kbjorke@gmail.com>
2013-09-27 15:24:18 +0200

    Added test for eigenvectormethod.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <unittest++/UnitTest++.h>
 #include <iostream>
+#include <cmath>
 #include "jacobi_algorithm.h"
 
 using namespace std;
@@ -62,7 +63,7 @@ TEST(jacobi_algorithm)
     A[2][1] = -1;
     A[2][2] = 5;
 
-    jacobi_algorithm(n, A, epsilon);
+    jacobi_algorithm(n, A, 0, epsilon);
 
     /*
     for( i = 0; i < n; i++ ){
@@ -84,3 +85,68 @@ TEST(jacobi_algorithm)
     delete[] A;
 }
 
+TEST(jacobi_eigenvectors)
+{
+    int i;
+    int j;
+    int n = 3;
+
+    double epsilon = 1e-8;
+
+    double **A = new double*[n];
+    for( i = 0; i < n; i++ ){
+        A[i] = new double[n];
+    }
+    double **V = new double*[n];
+    for( i = 0; i < n; i++ ){
+        V[i] = new double[n];
+    }
+
+
+    A[0][0] = 6;
+    A[0][1] = -2;
+    A[0][2] = -1;
+    A[1][0] = -2;
+    A[1][1] = 6;
+    A[1][2] = -1;
+    A[2][0] = -1;
+    A[2][1] = -1;
+    A[2][2] = 5;
+
+    for( i = 0; i < n; i++ ){
+        for( j = 0; j < n; j++ ){
+            if( i == j ){
+                V[i][j] = 1;
+            }
+            else{
+                V[i][j] = 0;
+            }
+        }
+    }
+
+    jacobi_algorithm(n, A, V, 1e-8);
+
+    CHECK( ( fabs(V[0][0]) - 1/sqrt(3) < epsilon ) &&
+           ( fabs(V[1][0]) - 1/sqrt(3) < epsilon ) &&
+           ( fabs(V[2][0]) - 1/sqrt(3) < epsilon ) &&
+
+           ( fabs(V[0][1]) - 1/sqrt(2) < epsilon ) &&
+           ( fabs(V[1][1]) - 1/sqrt(2) < epsilon ) &&
+           ( fabs(V[2][1]) - 0 < epsilon ) &&
+
+           ( fabs(V[0][2]) - 1/sqrt(6) < epsilon ) &&
+           ( fabs(V[1][2]) - 1/sqrt(6) < epsilon ) &&
+           ( fabs(V[2][2]) - 2/sqrt(6) < epsilon ) );
+
+    for( i = 0; i < n; i++ ){
+        delete[] A[i];
+    }
+    delete[] A;
+    for( i = 0; i < n; i++ ){
+        delete[] V[i];
+    }
+    delete[] V;
+
+
+
+}
