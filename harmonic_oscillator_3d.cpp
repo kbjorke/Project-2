2a78e9e88076ff6d4755ad57a7f27a4a883d46d8
Kristian Bjørke <kbjorke@gmail.com>
2013-10-01 21:39:02 +0200

    Added functionality to the class, an initialize and a solve method. Not complete.

--- harmonic_oscillator_3d.cpp
+++ harmonic_oscillator_3d.cpp
@@ -1,44 +1,122 @@
 #include "harmonic_oscillator_3d.h"
+#include "jacobi_algorithm.h"
+#include <cmath>
 
-Harmonic_Oscillator_3d::Harmonic_Oscillator_3d(int n_step, double rho_max,
-                                               bool interaction, double omega_r)
+#include <iostream>
+
+using namespace std;
+
+Harmonic_Oscillator_3d::Harmonic_Oscillator_3d(double **A_matrix, double **P_matrix,
+                                               bool interaction)
 {
-    rho_min = 0;
-    h = ( rho_max - rho_min )/ n_steps;
+    A = A_matrix;
+    if( P_matrix != 0 ){
+        P = P_matrix;
+        eigenvectors = true;
+    }
+    else{
+        eigenvectors = false;
+    }
+    interact = interaction;
 }
 
-void Harmonic_Oscillator_3d::solve(bool eigenvectors)
+void Harmonic_Oscillator_3d::initialize(int n_steps, double rho_max,
+                                        double omega_r, double **P_matrix)
 {
+    n = n_steps;
+
+    if( P_matrix != 0){
+        P = P_matrix;
+        eigenvectors = true;
+    }
+
+    if( omega_r != 0){
+        interact = true;
+    }
+
     int i;
     int j;
 
-    double **A = new double*[n_steps];
-    for( i = 0; i < n_steps; i++ ){
-        A[i] = new double[n_steps];
-    }
+    double omega_r2;
+
+    rho_min = 0;
+
+    omega_r2 = omega_r*omega_r;
+
+    h = ( rho_max - rho_min )/n_steps;
 
     e = -1/(h*h);
 
-    A[0][0] = -2*e;
-
-    if( interaction ){
-        for( i = 1; i < n_steps; i++ ){
+    if( interact ){
+        for( i = 0; i < n_steps; i++ ){
             rho = rho_min + i*h;
-            d = 2/(h*h) + omega_r*omega_r*rho*rho + 1/rho;
+            if( rho > 0 ){
+                d = -2*e + omega_r2*rho*rho + 1/rho;
+            }
+            else{
+                d = -2*e;
+            }
 
-            A[i][i] = d;
-            A[i-1][i] = e;
-            A[i][i-1] = e;
+            for( j = 0; j < n_steps; j++){
+                if( i == j ){
+                        A[i][j] = d;
+                }
+                else if( fabs( i - j ) == 1){
+                    A[i][j] = e;
+                }
+                else if( fabs( i - j ) > 1){
+                    A[i][j] = 0;
+                }
+            }
         }
     }
-    else{
-        for( i = 1; i < n_steps; i++ ){
-            rho = rho_min + i*h;
-            d = 2/(h*h) + rho*rho;
 
-            A[i][i] = d;
-            A[i-1][i] = e;
-            A[i][i-1] = e;
+    else{
+        for( i = 0; i < n_steps; i++ ){
+            rho = rho_min + i*h;
+            d = -2*e + rho*rho;
+
+            for( j = 0; j < n_steps; j++){
+                if( i == j ){
+                        A[i][j] = d;
+                }
+                else if( fabs( i - j ) == 1){
+                    A[i][j] = e;
+                }
+                else if( fabs( i - j ) > 1){
+                    A[i][j] = 0;
+                }
+            }
         }
+    }
+
+    if( eigenvectors ){
+        for( i = 0; i < n_steps; i++ ){
+            for( j = 0; j < n_steps; j++ ){
+                if( i == j ){
+                    P[i][j] = 1;
+                }
+                else{
+                    P[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+void Harmonic_Oscillator_3d::solve(double *eigenvalues)
+{
+    int i;
+
+    if( eigenvectors ){
+        jacobi_algorithm(n, A, P);
+    }
+    else{
+        jacobi_algorithm(n, A);
+    }
+
+    for( i = 0; i < n; i++ ){
+        eigenvalues[i] = A[i][i];
     }
 }
--- harmonic_oscillator_3d.h
+++ harmonic_oscillator_3d.h
@@ -3,27 +3,28 @@
 
 class Harmonic_Oscillator_3d
 {
+    int n;
+
     double **A;
     double **P;
 
-    int n_steps;
-
     double rho_min;
-    double rho_max;
     double h;
 
-    double omega_r;
 
     double rho;
     double d;
     double e;
 
-    bool interaction;
+    bool interact;
+    bool eigenvectors;
 
 public:
-    Harmonic_Oscillator_3d(int n_step, double rho_max, bool interaction = false,
-                           double omega_r = 0);
-    void solve (bool eigenvectors = false);
+    Harmonic_Oscillator_3d(double **A_matrix, double **P_matrix = 0,
+                           bool interaction = false);
+    void initialize(int n_steps, double rho_max, double omega_r = 0,
+                    double **P_matrix = 0);
+    void solve (double *eigenvalues);
 };
 
 #endif // HARMONIC_OSCILLATOR_3D_H
